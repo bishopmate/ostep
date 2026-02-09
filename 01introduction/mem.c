77e753fd6a165663c19a661e1bb51722fb4affef
Sashi Bhushan A Rajput <sashi.bhushanarajput@arcticwolf.com>
2026-02-09 17:44:01 +0000

    moved the explanation to bottom of program

--- 01introduction/mem.c
+++ 01introduction/mem.c
@@ -4,6 +4,22 @@
 #include <assert.h> // explicit for assert() prototype; avoids implicit declaration
 #include "common.h"
 
+int main(int argc, char *argv[])
+{
+    int *p = malloc(sizeof(int));                               // stdlib.h
+    assert(p != NULL);                                          // assert.h
+    printf("(%d) address pointed to by p: %p \n", getpid(), p); // stdio.h + unistd.h
+    *p = 0;
+    while (1)
+    {
+        Spin(1); // common.h
+        *p = *p + 1;
+        printf("(%d) p: %d\n", getpid(), *p); // stdio.h + unistd.h
+    }
+
+    return 0;
+}
+
 /*
 1. The Expectation (The Book)
 In Operating Systems: Three Easy Pieces, the author (Remzi) disables a specific security feature to demonstrate the concept of Virtual Memory.
@@ -29,19 +45,3 @@ uname -m: Gets your machine architecture (e.g., x86_64).
 ./mem: Runs your program.
 
 */
-
-int main(int argc, char *argv[])
-{
-    int *p = malloc(sizeof(int));                               // stdlib.h
-    assert(p != NULL);                                          // assert.h
-    printf("(%d) address pointed to by p: %p \n", getpid(), p); // stdio.h + unistd.h
-    *p = 0;
-    while (1)
-    {
-        Spin(1); // common.h
-        *p = *p + 1;
-        printf("(%d) p: %d\n", getpid(), *p); // stdio.h + unistd.h
-    }
-
-    return 0;
-}
